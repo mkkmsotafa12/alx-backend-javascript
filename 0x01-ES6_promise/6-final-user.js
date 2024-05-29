b1e70397245746338aed1f3991e37ff2aed4fa21
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-05-29 16:21:36 -0400

    6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName),
-    ]).then((results) => results.map((result) => ({
-        status: result.status,
-        value: result.reason || result.value,
-    })));
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    return Promise
+        .allSettled([userPromise, photoPromise])
+        .then((results) => results.map((result) => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result.value : String(result.reason),
+        })));
 }
