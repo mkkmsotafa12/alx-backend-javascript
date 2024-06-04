ef874a5d2c93c0060d98fc143e377cecac58672d
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-04 16:10:01 -0400

    1-get_list_student_ids

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(getListStudents) {
-  if (typeof getListStudents !== "object") {
+  if (typeof getListStudents !== 'object') {
     return [];
   }
   const listStudents = getListStudents;
