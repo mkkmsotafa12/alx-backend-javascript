8cc67a2e05c23e86d84067a304c80dbe93f64536
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-07 15:43:31 -0400

    8. Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (typeof set !== 'object' || typeof startString !== 'string') {
+    return '';
+  }
+  if (!startString) {
+    return '';
+  }
+  let result = '';
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      result += `${item.slice(startString.length)}-`;
+    }
+  }
+  return result.slice(0, result.length - 1);
+}
