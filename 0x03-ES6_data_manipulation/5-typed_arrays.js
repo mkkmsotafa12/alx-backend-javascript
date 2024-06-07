bafa2a82156fcf12b97e86aeb986ef77014b5ba0
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-07 15:35:28 -0400

    5. Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    throw new Error("Position outside range");
+    throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
