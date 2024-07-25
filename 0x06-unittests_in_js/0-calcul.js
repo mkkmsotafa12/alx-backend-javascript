b498dfc36e067032a5129b6a9b59a031dabf2e71
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-07-25 14:59:24 -0400

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,9 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+const calculateNumber = (a, b) => {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new TypeError("Both arguments must be numbers.");
+  }
+
+  return Math.round(a) + Math.round(b);
+};
 
 module.exports = calculateNumber;
